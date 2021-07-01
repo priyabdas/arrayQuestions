5b7aed226593cd504f24744f46095073f9c2e8ba
Priyabrata Das <pbrt.das@gmail.com>
2021-07-01 19:08:22 +0530

    triple sum python

--- tripleSum.py
+++ tripleSum.py
@@ -1,27 +1,31 @@
 
 
 def tripleSum (arr, targetSum):
+
     alen = len(arr)
     arr.sort()
-    for i in range (alen):
+    res = []
+    for i in range ( alen):
         start = i+1
         end = alen-1
         while start < end:
             currentSum = arr[i] + arr[start] + arr[end]
             if currentSum == targetSum:
+                res.append([arr[i] , arr[start] , arr[end]])
                 start +=1
                 end   -=1
-                print (arr[i] , arr[start] , arr[end])
             elif currentSum < targetSum:
                 start +=1
             else:
                 end -= 1
-
+    return res
 
 def main():
     arr = [1, 9, 15, 4, 6, 5, 7, 8, 2, 3]
     targetSum = 18
-    tripleSum(arr, targetSum)
+    res = tripleSum(arr, targetSum)
+    print (res)
+
 
 if __name__ == "__main__":
     main()
